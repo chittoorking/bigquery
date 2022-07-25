a94c0c4d802c12156bad929e59325c6afc91ecfb
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-07-25 09:08:00 +0530

    Create big query for data analysts 2.sql

---
+++ big query for data analysts 2.sql
@@ -0,0 +1,14 @@
+#standardSQL
+# copy one day of ecommerce data to explore
+CREATE OR REPLACE TABLE ecommerce.all_sessions_raw_20170801
+#schema
+(
+  fullVisitorId STRING OPTIONS(description="Unique visitor ID"),
+  channelGrouping STRING OPTIONS(description="Channel e.g. Direct, Organic, Referral...")
+)
+ OPTIONS(
+   description="Raw data from analyst team into our dataset for 08/01/2017"
+ ) AS
+ SELECT fullVisitorId,channelGrouping FROM `data-to-insights.ecommerce.all_sessions_raw`
+ WHERE date = '20170801'  #56,989 records
+;
